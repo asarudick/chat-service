3d9f7e47a11305c1f67986389d587c1183f949e6
Asa Rudick <Shabonkerz@gmail.com>
2015-12-06 16:06:32 -0600

    add additional redis connection information

--- src/redis/clients.js
+++ src/redis/clients.js
@@ -8,23 +8,54 @@ Promise.promisifyAll(redis.Multi.prototype);
 
 //  Redis pub/sub clients.
 var pub = redis.createClient(config.redis.pub);
-pub.on('connect', () => {
-    winston.info(`Redis pub client connected to ${pub.address}`);
-});
+
+pub
+	.on('connect', () => {
+		winston.info(`Redis pub client connected to ${pub.address}.`);
+	})
+	.on('ready', () => {
+		winston.info(`Redis pub client is ready. Server redis version: ${pub.server_info.redis_version}`);
+	})
+	.on('reconnect', (delay, attempt) => {
+		winston.info(`Redis pub client reconnecting in ${delay}ms. Attempt #${attempt}.`);
+	})
+	.on('end', () => {
+		winston.info(`Redis pub client disconnected.`);
+	});
 
 var sub = redis.createClient(config.redis.sub);
 
-sub.on('connect', () => {
-    winston.info(`Redis sub client connected to ${sub.address}`);
-});
+sub
+	.on('connect', () => {
+		winston.info(`Redis sub client connected to ${sub.address}.`);
+	})
+	.on('ready', () => {
+		winston.info(`Redis sub client is ready. Server redis version: ${sub.server_info.redis_version}`);
+	})
+	.on('reconnect', (delay, attempt) => {
+		winston.info(`Redis sub client reconnecting in ${delay}ms. Attempt #${attempt}.`);
+	})
+	.on('end', () => {
+		winston.info(`Redis sub client disconnected.`);
+	});
 
 sub.subscribe('global');
 
 // Redis cache store.
 var store = redis.createClient(config.redis.store);
 
-store.on('connect', () => {
-    winston.info(`Redis store client connected to ${store.address}`);
-});
+store
+	.on('connect', () => {
+		winston.info(`Redis store client connected to ${store.address}.`);
+	})
+	.on('ready', () => {
+		winston.info(`Redis store client is ready. Server redis version: ${store.server_info.redis_version}`);
+	})
+	.on('reconnect', (delay, attempt) => {
+		winston.info(`Redis store client reconnecting in ${delay}ms. Attempt #${attempt}.`);
+	})
+	.on('end', () => {
+		winston.info(`Redis store client disconnected.`);
+	});
 
 export { pub, sub, store };
