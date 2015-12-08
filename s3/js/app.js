1a4119509d35b9f25624b1cf6d38e47f00443f12
Asa Rudick <Shabonkerz@gmail.com>
2015-12-08 12:43:29 -0600

    prompt for host instead of baking in host

--- s3/js/app.js
+++ s3/js/app.js
@@ -10,10 +10,10 @@ $(function () {
 	var source = $("#message-template").html();
 	var template = Handlebars.compile(source);
 
-	var socket = io('http://52.70.145.9:3001');
 	var textInput = $('#m');
 	var messages = $('#messages');
 	var scrollBody = $('html, body');
+	var hostModal = $('#hostModal');
 
 	function writeLine(message) {
 		messages.append(template({
@@ -27,35 +27,52 @@ $(function () {
 		);
 	};
 
-	$('form').submit(function () {
-		socket.emit('chat.data', textInput.val());
-		textInput.val('');
-		return false;
+	function startChat(host) {
+		var socket = io(host);
+
+		socket.on('chat.data', function (msg) {
+			writeLine(msg);
+		});
+
+		socket.on('disconnect', function () {
+			writeLine('You have been disconnected.');
+		});
+		socket.on('error', function (error) {
+			writeLine('Connection error: ' + error);
+		});
+		socket.on('connect', function () {
+			writeLine('You have connected to the chat service.');
+		});
+		socket.on('reconnecting', function (attempt) {
+			writeLine('Attempting to reconnect. Attempt #' + attempt);
+		});
+		socket.on('reconnect', function (attempt) {
+			writeLine('Reconnected after ' + attempt + ' attempts.');
+		});
+		socket.on('reconnect_error', function (error) {
+			writeLine('Reconnect error: ' + error);
+		});
+		socket.on('reconnect_failed', function () {
+			writeLine('Unable to reconnect.');
+		});
+
+		$('form').submit(function () {
+			socket.emit('chat.data', textInput.val());
+			textInput.val('');
+			return false;
+		});
+	}
+
+
+
+	hostModal.on('hidden.bs.modal', function () {
+		var host = $('#host').val();
+		if (host.search(/^https?/) === -1)
+		{
+			host = 'http://' + host;
+		}
+		startChat(host);
 	});
 
-	socket.on('chat.data', function (msg) {
-		writeLine(msg);
-	});
-
-	socket.on('disconnect', function () {
-		writeLine('You have been disconnected.');
-	});
-	socket.on('error', function (error) {
-		writeLine('Connection error: ' + error);
-	});
-	socket.on('connect', function () {
-		writeLine('You have connected to the chat service.');
-	});
-	socket.on('reconnecting', function (attempt) {
-		writeLine('Attempting to reconnect. Attempt #' + attempt);
-	});
-	socket.on('reconnect', function (attempt) {
-		writeLine('Reconnected after ' + attempt + ' attempts.');
-	});
-	socket.on('reconnect_error', function (error) {
-		writeLine('Reconnect error: ' + error);
-	});
-	socket.on('reconnect_failed', function () {
-		writeLine('Unable to reconnect.');
-	});
+	hostModal.modal();
 });
