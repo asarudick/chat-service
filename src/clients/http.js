f8cb5694e32766394a9f8606ac8e3dac27780f9d
Asa Rudick <Shabonkerz@gmail.com>
2015-12-08 18:41:02 -0600

    update http client to send notifications and room messages different from base class

--- src/clients/http.js
+++ src/clients/http.js
@@ -6,7 +6,7 @@ import winston from 'winston';
 
 export default class HttpChatClient extends BaseChatClient {
 
-	constructor (client) {
+	constructor(client) {
 		super();
 
 		this._client = client;
@@ -15,11 +15,11 @@ export default class HttpChatClient extends BaseChatClient {
 		this._registerEvents();
 	}
 
-	_writeLine (message) {
+	_writeLine(message) {
 		this._client.emit('chat.data', message);
 	}
 
-	_registerEvents () {
+	_registerEvents() {
 		this._client.on('chat.data', (data) => {
 			super._onData(data);
 		});
@@ -32,11 +32,46 @@ export default class HttpChatClient extends BaseChatClient {
 
 	}
 
-	static create (client) {
+	/**
+	 * Handles any message from the current room.
+	 * @method _onRoomMessage
+	 * @param  {Object}          message The message sent in the room.
+	 */
+	_onRoomMessage(message) {
+
+		// Filter out any messages from this user.
+		if (message.name === this.user.name) {
+			return;
+		}
+
+		this._client.emit('room.message', message);
+	}
+
+	/**
+	 * Handles any room notification.
+	 * @method _onNotificationMessage
+	 * @param  {Object}               message The notification message.
+	 */
+	// TODO: Use an object to map notifcation types with their respective handler.
+	// TODO: Refactor me.
+	_onNotificationMessage(message) {
+		if (message.notificationType === 'userJoin') {
+			if (message.user !== this.user.name) {
+				this._client.emit('room.notification', {text: ` * a new user joined ${this.user.room}: ${message.user}`, type: message.notificationType });
+			}
+		} else if (message.notificationType === 'userLeave') {
+			if (message.user === this.user.name) {
+				this._client.emit('room.notification', {text: ` * user has left ${this.user.room}: ${message.user} (** this is you)`, type: message.notificationType });
+			}
+			this._client.emit('room.notification', {text:` * user has left ${this.user.room}: ${message.user} (** this is you)`, type: message.notificationType });
+		}
+	}
+
+	static create(client) {
 		return new HttpChatClient(client);
 	}
 
-	async disconnect () {
+	async disconnect() {
 		await super.disconnect();
 		this._client.disconnect();
 		this._client.removeAllListeners();
